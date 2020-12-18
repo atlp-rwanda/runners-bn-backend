9dfa131abac09ad8106cc5d03cb4707ffe603a66
Nyagatare James <nyagatarejames@gmail.com>
2020-12-18 07:57:52 +0200

    chore(eslint): Configure eslint in the project
    - Add an eslint package in the project.
    - Add eslint extension to vsCode
    - Add prettier configuration to match eslint
    
    [Delivers #176153376]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import { config } from "dotenv";
-import routes from "./routes";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+import routes from './routes';
 
 config();
 
@@ -11,12 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
-app.use("/*", (_req, res) => {
-  res.status(404).json({ message: "Route Not Found" });
+app.use('/*', (_req, res) => {
+  res.status(404).json({ message: 'Route Not Found' });
 });
 
 const port = process.env.PORT || 5000;
